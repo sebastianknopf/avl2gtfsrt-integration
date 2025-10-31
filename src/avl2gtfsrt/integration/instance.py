0f0d51012b999401f410388b7c9dd35728da0618
mail@sebastian-knopf.de <mail@sebastian-knopf.de>
2025-10-31 11:03:38 +0100

    added proper error handling for AvlDataInstance main loop

--- src/avl2gtfsrt/integration/instance.py
+++ src/avl2gtfsrt/integration/instance.py
@@ -45,52 +45,61 @@ class AvlDataInstance:
         self._should_run.clear()
 
     def _run_internal(self) -> None:
+        
         # startup IoM client
         self._iom.start()
 
         # main loop, run adapter logic here ...
         while self._should_run.is_set():
-            # call configured adapter in order to get all current vehicle positions
-            logging.info(f"{self.id}/{self.__class__.__name__}: Loading current vehicles ...")
-            vehicles_result: list[Vehicle] = self._adapter.get_vehicles()
 
-            # log on and add newly discovered vehicles ...
-            for vehicle in vehicles_result:
-                if vehicle not in self._vehicles:
-                    logging.debug(f"{self.id}/{self.__class__.__name__}: Vehicle \"{vehicle.vehicle_ref}\" discovered.")
-                    logging.info(f"{self.id}/{self.__class__.__name__}: Logging on vehicle \"{vehicle.vehicle_ref}\" ...")
+            try:
+                # call configured adapter in order to get all current vehicle positions
+                logging.info(f"{self.id}/{self.__class__.__name__}: Loading current vehicles ...")
+                vehicles_result: list[Vehicle] = self._adapter.get_vehicles()
 
-                    self._iom.log_on_vehicle(vehicle)
+                # log on and add newly discovered vehicles ...
+                for vehicle in vehicles_result:
+                    if vehicle not in self._vehicles:
+                        logging.debug(f"{self.id}/{self.__class__.__name__}: Vehicle \"{vehicle.vehicle_ref}\" discovered.")
+                        logging.info(f"{self.id}/{self.__class__.__name__}: Logging on vehicle \"{vehicle.vehicle_ref}\" ...")
 
-                    self._vehicles.append(vehicle)
+                        self._iom.log_on_vehicle(vehicle)
 
-            # log off and remove disappeared vehicles ...
-            for vehicle in self._vehicles:
-                if vehicle not in vehicles_result:
-                    logging.debug(f"{self.id}/{self.__class__.__name__}: Vehicle \"{vehicle.vehicle_ref}\" disappeared.")
-                    logging.info(f"{self.id}/{self.__class__.__name__}: Logging off vehicle \"{vehicle.vehicle_ref}\" ...")
+                        self._vehicles.append(vehicle)
+
+                # log off and remove disappeared vehicles ...
+                for vehicle in self._vehicles:
+                    if vehicle not in vehicles_result:
+                        logging.debug(f"{self.id}/{self.__class__.__name__}: Vehicle \"{vehicle.vehicle_ref}\" disappeared.")
+                        logging.info(f"{self.id}/{self.__class__.__name__}: Logging off vehicle \"{vehicle.vehicle_ref}\" ...")
+                        
+                        self._iom.log_off_vehicle(vehicle)
+
+                        self._vehicles.remove(vehicle)
+                        del self._vehicle_positions[vehicle]
+
+                # load and process vehicle positions for all vehicles
+                logging.info(f"{self.id}/{self.__class__.__name__}: Loading current vehicle positions of {len(vehicles_result)} vehicles ...")
+                vehicle_positions_result: list[VehiclePosition] = self._adapter.get_vehicle_positions()
+
+                for vehicle_position in vehicle_positions_result:
+                    reference_timestamp: int = int((datetime.now() - timedelta(seconds=150)).timestamp())
+                    last_vehicle_position: VehiclePosition|None = self._vehicle_positions[vehicle_position.vehicle.id] if vehicle_position.vehicle.id in self._vehicle_positions else None
                     
-                    self._iom.log_off_vehicle(vehicle)
+                    if vehicle_position.timestamp >= reference_timestamp and (last_vehicle_position is None or vehicle_position.latitude != last_vehicle_position.latitude or vehicle_position.longitude != last_vehicle_position.longitude):
+                        logging.info(f"{self.id}/{self.__class__.__name__}: Publishing GNSS position update for vehicle \"{vehicle.vehicle_ref}\" ...")
+                        self._iom.publish_gnss_position_update(vehicle_position)
 
-                    self._vehicles.remove(vehicle)
-                    del self._vehicle_positions[vehicle]
+                        self._vehicle_positions[vehicle_position.vehicle.id] = vehicle_position 
+                    else:
+                        logging.info(f"{self.id}/{self.__class__.__name__}: No actual GNSS position for vehicle \"{vehicle.vehicle_ref}\".")
 
-            # load and process vehicle positions for all vehicles
-            logging.info(f"{self.id}/{self.__class__.__name__}: Loading current vehicle positions of {len(vehicles_result)} vehicles ...")
-            vehicle_positions_result: list[VehiclePosition] = self._adapter.get_vehicle_positions()
-
-            for vehicle_position in vehicle_positions_result:
-                reference_timestamp: int = int((datetime.now() - timedelta(seconds=150)).timestamp())
-                last_vehicle_position: VehiclePosition|None = self._vehicle_positions[vehicle_position.vehicle.id] if vehicle_position.vehicle.id in self._vehicle_positions else None
+            except Exception as ex:
+                logging.error(ex)
+            finally:
                 
-                if vehicle_position.timestamp >= reference_timestamp and (last_vehicle_position is None or vehicle_position.latitude != last_vehicle_position.latitude or vehicle_position.longitude != last_vehicle_position.longitude):
-                    logging.info(f"{self.id}/{self.__class__.__name__}: Publishing GNSS position update for vehicle \"{vehicle.vehicle_ref}\" ...")
-                    self._iom.publish_gnss_position_update(vehicle_position)
-
-                    self._vehicle_positions[vehicle_position.vehicle.id] = vehicle_position 
-
-            # wait for the adapter configured timespan until the next request
-            time.sleep(self._adapter.interval)
+                # wait for the adapter configured timespan until the next request
+                time.sleep(self._adapter.interval)
 
         # shutdown the instance here ...
         for vehicle in self._vehicles:
