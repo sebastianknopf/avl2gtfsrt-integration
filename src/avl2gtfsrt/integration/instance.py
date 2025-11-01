992d0ee9e264fb271d45bad66102e5186d2d323d
mail@sebastian-knopf.de <mail@sebastian-knopf.de>
2025-11-01 08:18:10 +0100

    improved AvlDataInstance to log vehicles dynamically on and off based on data timestamps

--- src/avl2gtfsrt/integration/instance.py
+++ src/avl2gtfsrt/integration/instance.py
@@ -57,61 +57,68 @@ class AvlDataInstance:
                 logging.info(f"{self.id}/{self.__class__.__name__}: Loading current vehicles ...")
                 vehicles_result: list[Vehicle] = self._adapter.get_vehicles()
 
-                # log on and add newly discovered vehicles ...
+                # sync vehicle list
+                # log off vehicles which are not appearing in 
+                # adapters vehicle list anymore 
                 for vehicle in vehicles_result:
-                    # verify log on state of the vehicle
-                    # use vehicle of this instance as only this has the log on state!
-                    instance_vehicle: Vehicle|None = next((v for v in self._vehicles if v.vehicle_ref == vehicle.vehicle_ref), None)
-                    if vehicle not in self._vehicles or instance_vehicle is not None and not instance_vehicle.is_logged_on:
-                        logging.debug(f"{self.id}/{self.__class__.__name__}: Vehicle \"{vehicle.vehicle_ref}\" discovered.")
-                        logging.info(f"{self.id}/{self.__class__.__name__}: Logging on vehicle \"{vehicle.vehicle_ref}\" ...")
+                    if vehicle not in self._vehicles:
+                        self._vehicles.append(vehicle)
 
-                        if vehicle not in self._vehicles:
-                            self._vehicles.append(vehicle)
-
-                        try:
-                            self._iom.log_on_vehicle(vehicle)
-
-                            vehicle.is_logged_on = True
-                        except Exception as ex:
-                            logging.error(ex)
-
-                # log off and remove disappeared vehicles ...
                 for vehicle in self._vehicles:
                     if vehicle not in vehicles_result:
-                        logging.debug(f"{self.id}/{self.__class__.__name__}: Vehicle \"{vehicle.vehicle_ref}\" disappeared.")
+                        logging.info(f"{self.id}/{self.__class__.__name__}: Vehicle \"{vehicle.vehicle_ref}\" disappeared.")
                         logging.info(f"{self.id}/{self.__class__.__name__}: Logging off vehicle \"{vehicle.vehicle_ref}\" ...")
                         
                         self._vehicles.remove(vehicle)
                         del self._vehicle_positions[vehicle]
                         
+                        if vehicle.is_logged_on:
+                            try:
+                                self._iom.log_off_vehicle(vehicle)
+                            except Exception as ex:
+                                logging.error(ex)
+
+                # load and process vehicle positions for all vehicles
+                # log on vehicles if they have delivered new data
+                # log off vehicles which have not delivered data for the last 60 minutes
+                logging.info(f"{self.id}/{self.__class__.__name__}: Loading current vehicle positions of {len(vehicles_result)} vehicles ...")
+                vehicle_positions_result: list[VehiclePosition] = self._adapter.get_vehicle_positions()
+                vehicle_positions_published: bool = False
+
+                for vehicle_position in vehicle_positions_result:
+                    reference_timestamp: int = int((datetime.now() - timedelta(minutes=5)).timestamp())
+                    last_vehicle_position: VehiclePosition|None = self._vehicle_positions[vehicle_position.vehicle.id] if vehicle_position.vehicle.id in self._vehicle_positions else None
+
+                    if vehicle_position.timestamp >= reference_timestamp and (last_vehicle_position is None or vehicle_position.latitude != last_vehicle_position.latitude or vehicle_position.longitude != last_vehicle_position.longitude):
+                        if not vehicle_position.vehicle.is_logged_on:
+                            try:
+                                logging.info(f"{self.id}/{self.__class__.__name__}: Logging on vehicle \"{vehicle.vehicle_ref}\" ...")
+                                
+                                self._iom.log_on_vehicle(vehicle_position.vehicle)
+                                vehicle_position.vehicle.is_logged_on = True
+                            except Exception as ex:
+                                logging.error(ex)
+
+                                continue
+                        
+                        logging.info(f"{self.id}/{self.__class__.__name__}: Publishing GNSS position update for vehicle \"{vehicle.vehicle_ref}\" ...")
+                        self._iom.publish_gnss_position_update(vehicle_position)
+
+                        self._vehicle_positions[vehicle_position.vehicle.id] = vehicle_position 
+
+                        vehicle_positions_published = True
+                        
+                    elif vehicle_position.timestamp < reference_timestamp and vehicle_position.vehicle.is_logged_on:
                         try:
-                            self._iom.log_off_vehicle(vehicle)
+                            logging.info(f"{self.id}/{self.__class__.__name__}: Logging off vehicle \"{vehicle.vehicle_ref}\" ...")
+                                
+                            self._iom.log_off_vehicle(vehicle_position.vehicle)
+                            vehicle_position.vehicle.is_logged_on = False
                         except Exception as ex:
                             logging.error(ex)
 
-                # load and process vehicle positions for all vehicles
-                logging.info(f"{self.id}/{self.__class__.__name__}: Loading current vehicle positions of {len(vehicles_result)} vehicles ...")
-                vehicle_positions_result: list[VehiclePosition] = self._adapter.get_vehicle_positions()
-
-                for vehicle_position in vehicle_positions_result:
-
-                    # verify log on state of the vehicle
-                    # use vehicle of this instance as only this has the log on state!
-                    instance_vehicle: Vehicle|None = next((v for v in self._vehicles if v.vehicle_ref == vehicle_position.vehicle.vehicle_ref), None)
-                    if instance_vehicle is not None and instance_vehicle.is_logged_on:
-                        reference_timestamp: int = int((datetime.now() - timedelta(seconds=150)).timestamp())
-                        last_vehicle_position: VehiclePosition|None = self._vehicle_positions[vehicle_position.vehicle.id] if vehicle_position.vehicle.id in self._vehicle_positions else None
-                        
-                        if vehicle_position.timestamp >= reference_timestamp and (last_vehicle_position is None or vehicle_position.latitude != last_vehicle_position.latitude or vehicle_position.longitude != last_vehicle_position.longitude):
-                            logging.info(f"{self.id}/{self.__class__.__name__}: Publishing GNSS position update for vehicle \"{vehicle.vehicle_ref}\" ...")
-                            self._iom.publish_gnss_position_update(vehicle_position)
-
-                            self._vehicle_positions[vehicle_position.vehicle.id] = vehicle_position 
-                        else:
-                            logging.info(f"{self.id}/{self.__class__.__name__}: No actual GNSS position for vehicle \"{vehicle.vehicle_ref}\".")
-                    else:
-                        logging.info(f"{self.id}/{self.__class__.__name__}: GNSS position for vehicle \"{vehicle.vehicle_ref}\" discarded, vehicle could not be logged on.")
+                if not vehicle_positions_published:
+                    logging.info(f"{self.id}/{self.__class__.__name__}: No actual vehicle positions found.")
 
             except Exception as ex:
                 logging.error(ex)
@@ -120,11 +127,13 @@ class AvlDataInstance:
                 time.sleep(self._adapter.interval)
 
         # shutdown the instance here ...
+        # log off all actively monitored vehicles
         for vehicle in self._vehicles:
-            try:
-                logging.info(f"{self.id}/{self.__class__.__name__}: Logging off vehicle \"{vehicle.vehicle_ref}\" ...")
-                self._iom.log_off_vehicle(vehicle)
-            except Exception as ex:
-                logging.error(ex)
+            if vehicle.is_logged_on:
+                try:
+                    logging.info(f"{self.id}/{self.__class__.__name__}: Logging off vehicle \"{vehicle.vehicle_ref}\" ...")
+                    self._iom.log_off_vehicle(vehicle)
+                except Exception as ex:
+                    logging.error(ex)
 
         self._iom.terminate()
