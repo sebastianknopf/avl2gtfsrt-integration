3095a28144480681486455e34be9d20e68fe9e72
mail@sebastian-knopf.de <mail@sebastian-knopf.de>
2026-02-04 22:54:43 +0100

    first working implementation for Traccar websocket messages

--- src/avl2gtfsrt/integration/adapter/traccar/adapter.py
+++ src/avl2gtfsrt/integration/adapter/traccar/adapter.py
@@ -1,3 +1,4 @@
+import json
 import logging
 import urllib
 import time
@@ -19,10 +20,84 @@ class TraccarAdapter(RealtimeAdapter):
         logging.info(f"{self.instance_id}/{self.__class__.__name__}: WebSocket connected.")
 
     def _on_message(self, ws: WebSocketApp, message: str) -> None:
-        if self.on_vehicle_log_off is not None:
-            self.on_vehicle_log_off(None)
-        
-        logging.debug(f"{self.instance_id}/{self.__class__.__name__}: Received message: {message}")
+        data: dict = json.loads(message)
+
+        if 'devices' in data:
+            for device in data['devices']:
+                device_id: str = device['id']
+                vehicle: Vehicle|None = next((v for v in self._vehicles if v.id == device_id), None)
+
+                # add vehicle to tracking if not already present
+                if vehicle is None:
+                    if 'vehicleId' not in device['attributes']:
+                        logging.warning(f"{self.instance_id}/{self.__class__.__name__}: Device {device_id} has no vehicleId attribute. Using name as fallback.")
+                    
+                    vehicle = Vehicle(
+                        id=device_id,
+                        vehicle_ref=device['attributes']['vehicleId'] if 'vehicleId' in device['attributes'] else device['name']
+                    )
+
+                    self._vehicles.append(vehicle)
+
+        elif 'positions' in data:
+            for position in data['positions']:
+                device_id: str = position['deviceId']
+                vehicle: Vehicle|None = next((v for v in self._vehicles if v.id == device_id), None)
+
+                # ignore position if device / vehicle is not known
+                if vehicle is None:
+                    logging.warning(f"{self.instance_id}/{self.__class__.__name__}: Received position for unknown device {device_id}. Position was ignored.")
+                    continue
+
+                # ignore positon if valid flag is false
+                if not position['valid']:
+                    logging.info(f"{self.instance_id}/{self.__class__.__name__}: Received invalid position for device {device_id}. Position was ignored.")
+                    continue
+
+                vehicle_position: VehiclePosition = VehiclePosition(
+                    vehicle=vehicle,
+                    latitude=position['latitude'],
+                    longitude=position['longitude'],
+                    timestamp=int(datetime.fromisoformat(position['fixTime']).timestamp())
+                )
+
+                # check whether the vehicle is already logged on
+                # if not, trigger a log on at first
+                if not vehicle.is_logged_on:
+                    vehicle.is_logged_on = True
+
+                    # trigger the callback function if configured
+                    if self.on_vehicle_log_on is not None:
+                        self.on_vehicle_log_on(vehicle)
+
+                # trigger the callback function if configured
+                if self.on_vehicle_physical_position_update is not None:
+                    self.on_vehicle_physical_position_update(vehicle_position)
+
+        elif 'events' in data:
+            for event in data['events']:
+                event_type: str = event['type']
+                device_id: str = event['deviceId']
+                
+                vehicle: Vehicle|None = next((v for v in self._vehicles if v.id == device_id), None)
+                if vehicle is None:
+                    logging.warning(f"{self.instance_id}/{self.__class__.__name__}: Received event for unknown device {device_id}. Event was ignored.")
+                    continue
+
+                if event_type == 'deviceOnline':
+                    if not vehicle.is_logged_on:
+                        vehicle.is_logged_on = True
+
+                        # trigger the callback function if configured
+                        if self.on_vehicle_log_on is not None:
+                            self.on_vehicle_log_on(vehicle)
+
+                elif event_type == 'deviceInactive':
+                    vehicle.is_logged_on = False
+
+                    # trigger the callback function if configured
+                    if self.on_vehicle_log_off is not None:
+                        self.on_vehicle_log_off(vehicle)
 
     def _on_error(self, ws: WebSocketApp, error: Exception) -> None:
         logging.error(f"{self.instance_id}/{self.__class__.__name__}: WebSocket error {str(error)}")
@@ -63,7 +138,7 @@ class TraccarAdapter(RealtimeAdapter):
 
             logging.info(f"{self.instance_id}/{self.__class__.__name__}: Starting WebSocket connection for realtime updates ...")
             self._ws: WebSocketApp = WebSocketApp(
-                self._get_url('socket').replace('https', 'wss'),
+                self._get_url('socket').replace('https', 'wss').replace('http', 'ws'),
                 header={
                     'Cookie': f"JSESSIONID={self._login_token}"
                 },
